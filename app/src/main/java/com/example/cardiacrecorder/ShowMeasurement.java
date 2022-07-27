c9eeae835873ccefc1a2f2febad03286b63429ec
shihab-newaz <newaz1807023@stud.kuet.ac.bd>
2022-07-28 01:33:56 +0600

    ShowMeasurement.java modified

--- app/src/main/java/com/example/cardiacrecorder/ShowMeasurement.java
+++ app/src/main/java/com/example/cardiacrecorder/ShowMeasurement.java
@@ -7,29 +7,36 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShowMeasurement extends AppCompatActivity {
-    SQliteDBmanager sQliteDBmanager ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent=getIntent();
         setContentView(R.layout.activity_show_measurement);
+
         String date=intent.getStringExtra("creation_date");
-        TextView textView=findViewById(R.id.showTime);
-        textView.setText(date);
+        TextView showtime=findViewById(R.id.showTime);
+        showtime.setText(date);
+
         String time=intent.getStringExtra("creation_time");
-        TextView textView1=findViewById(R.id.showDate);
-        textView1.setText(time);
+        TextView showDate=findViewById(R.id.showDate);
+        showDate.setText(time);
+
         String systolic=intent.getStringExtra("systolic");
-        TextView textView2=findViewById(R.id.showSystolic);
-        textView2.setText(systolic);
+        TextView showSystolic=findViewById(R.id.showSystolic);
+        showSystolic.setText(systolic);
+
         String diastolic=intent.getStringExtra("diastolic");
-        TextView textView3=findViewById(R.id.showDiastolic);
-        textView3.setText(diastolic);
+        TextView showDiastolic=findViewById(R.id.showDiastolic);
+        showDiastolic.setText(diastolic);
+
         String heart_rate=intent.getStringExtra("heart_rate");
-        TextView textView4=findViewById(R.id.showBPM);
-        textView4.setText(heart_rate);
+        TextView showBPM=findViewById(R.id.showBPM);
+        showBPM.setText(heart_rate);
+
         String comments=intent.getStringExtra("comments");
-        TextView textView5=findViewById(R.id.showComment);
-        textView5.setText(comments);
+        TextView showComment=findViewById(R.id.showComment);
+        showComment.setText(comments);
+
     }
 }
